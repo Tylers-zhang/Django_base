47b4ad331d7147759c807867ca18006d20fdfc9a
Tylers-zhang <244605859@qq.com>
2022-05-31 06:31:13 -0700

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+1. wo men de mo xing lei xu yao ji cheng zi models.Model
+2. xi tong hui zi dong wei wo men tian jia yi ge zhu jian -- id
+3. zi duan
+
+    zi duan ming = model.lei xing( xuan xiang)
+    
+    zi duan ming qi shi jiu shi shu ju biao de zi duan ming
+    zi duan ming bu yao shiyong python , mysql deng guan jian zi
+    
+    char(M)
+    varchar(M)
+    M jiu shi xuan xiang
+"""
+
+
+class BookInfo(models.Model):
+    # id
+    name = models.CharField(max_length=10)
+
+    # renwu xin fuzhi guo lai , houqi jiang yuanli
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
