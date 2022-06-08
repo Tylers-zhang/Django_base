c6092a6b63c343f1663b80967b5d5d114751949c
Tylers-zhang <244605859@qq.com>
2022-06-08 02:30:39 -0700

    定义视图函数

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+shitu
+suowei de shitu jiushi python hanshu
+shitu hanshu you liangge yaoqiu 
+    1.shitu hanshu de diyige canshu jiushi jieshou qingqiu
+"""
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+# women qiwang yonghu shuru http://127.0.0.1:8000/index/
+def index(request):
+
+    return HttpResponse('ok')
